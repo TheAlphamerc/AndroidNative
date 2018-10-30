ef50339afcc0e585a1676ab210eb12bba3e3c754
Sonu Sharma <sonu.sahrma@mobiloitte.in>
2018-10-30 16:39:46 +0530

    Implement :- Hello, Android Multiscreen

--- NativeAndroid/MainActivity.cs
+++ NativeAndroid/MainActivity.cs
@@ -3,17 +3,49 @@ using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
+using System.Collections.Generic;
+using Android.Content;
 
 namespace NativeAndroid
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        static readonly List<string> phoneNumbers = new List<string>();
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+          
+
+            EditText phoneNumberText = FindViewById<EditText>(Resource.Id.textfield);
+            TextView translatedPhoneWord = FindViewById<TextView>(Resource.Id.Label);
+            Button translateButton = FindViewById<Button>(Resource.Id.button);
+            Button translationHistoryButton = FindViewById<Button>(Resource.Id.call);
+
+            string translatedNumber = string.Empty;
+            translateButton.Click += (sender, e) =>
+            {
+                 translatedNumber = MainClass.ToNumber(phoneNumberText.Text);
+                if (string.IsNullOrWhiteSpace(translatedNumber))
+                {
+                    translatedPhoneWord.Text = string.Empty;
+                }
+                else
+                {
+                    translatedPhoneWord.Text = translatedNumber;
+                    phoneNumbers.Add(translatedNumber);
+                    translationHistoryButton.Enabled = true;
+                }
+            };
+            translationHistoryButton.Click += (sender, e) =>
+            {
+                var intent = new Intent(this, typeof(TranslationHistoryActivity));
+                intent.PutStringArrayListExtra("phone_numbers", phoneNumbers);
+                StartActivity(intent);
+            };
         }
     }
 }
